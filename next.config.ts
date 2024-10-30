059560eb37e8c8e152803ad5322e828f48109f4e
Dimitar Yordanov <dimitar.yordanov11@outlook.com>
2024-10-30 23:31:50 +0200

    Add prettier, t3-env, and update packages

--- next.config.ts
+++ next.config.ts
@@ -1,5 +1,18 @@
 import type { NextConfig } from "next";
 
+import { createJiti } from "jiti";
+import { fileURLToPath } from "url";
+const jiti = createJiti(fileURLToPath(import.meta.url));
+
+async function loadEnv() {
+  await jiti.import("./src/env.js");
+}
+
+loadEnv().catch((e) => {
+  console.error(e);
+  process.exit(1);
+});
+
 const nextConfig: NextConfig = {
   /* config options here */
 };
