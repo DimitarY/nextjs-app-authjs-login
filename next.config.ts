acb9b295b408c04a3d3241c67d3810118a977d2b
Dimitar Yordanov <dimitar.yordanov11@outlook.com>
2024-11-08 20:03:32 +0200

    Update next and react, and remove jiti

--- next.config.ts
+++ next.config.ts
@@ -1,17 +1,5 @@
+import "@/env";
 import type { NextConfig } from "next";
-
-import { createJiti } from "jiti";
-import { fileURLToPath } from "url";
-const jiti = createJiti(fileURLToPath(import.meta.url));
-
-async function loadEnv() {
-  await jiti.import("./src/env.js");
-}
-
-loadEnv().catch((e) => {
-  console.error(e);
-  process.exit(1);
-});
 
 const nextConfig: NextConfig = {
   /* config options here */
